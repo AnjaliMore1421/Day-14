e8fdeffaea62eefa3e6d5b1b61f6ee3f667012f0
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-09 16:32:30 +0530

    Add Reports component for performance analytics

---
+++ nested-dashboard-routes/Reports.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Reports() {
+  return (
+    <div>
+      <h2>Reports</h2>
+      <p>Performance reports and analytics.</p>
+    </div>
+  );
+}
+
+export default Reports;
