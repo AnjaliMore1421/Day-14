7e6cba79f2f4f10b54c1cf080bd5074d2927f531
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-09 16:54:52 +0530

    Create Dashboard.js

---
+++ private-route-auth-dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="card">
+      <h2>Dashboard</h2>
+      <p>Welcome! This is a protected page.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
