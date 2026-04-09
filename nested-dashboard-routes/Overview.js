35697f86589c9703326a8298137e729183dd0a89
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-09 16:34:07 +0530

    Add Overview component for dashboard

---
+++ nested-dashboard-routes/Overview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Overview() {
+  return (
+    <div>
+      <h2>Overview</h2>
+      <p>This is dashboard overview page.</p>
+    </div>
+  );
+}
+
+export default Overview;
