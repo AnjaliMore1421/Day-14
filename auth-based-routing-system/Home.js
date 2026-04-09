e187e75fd919e8db0da1006d8987b7ee5a86eece
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-09 17:10:21 +0530

    Create Home.js

---
+++ auth-based-routing-system/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="page-card">
+      <h1>Auth Based Routing Demo</h1>
+      <p>Protected navigation using React Router v6</p>
+    </div>
+  );
+}
+
+export default Home;
