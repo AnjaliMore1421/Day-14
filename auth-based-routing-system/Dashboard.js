6d6ec855e219ae2ec414f86eb759a42994374cd1
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-09 17:09:04 +0530

    Create Dashboard.js

---
+++ auth-based-routing-system/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="page-card">
+      <h2>Dashboard</h2>
+      <p>Authentication successful.</p>
+      <p>Access granted to protected route.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
